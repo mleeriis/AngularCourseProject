47714832d392cbae71ffb2020937367e55a89ff5
mleeriis <mlee@riis.com>
2019-08-22 14:15:21 -0400

    Add Recipes Model and create array of recipes for use in Recipe-List Component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Recipe 1', 'Description of Recipe 1', 'imagePath'),
+    // tslint:disable-next-line:max-line-length
+    new Recipe('Tasty Recipe', 'A tasty recipe', 'https://www.simplyrecipes.com/wp-content/uploads/2019/08/summer-chicken-panzanella-Lead3-768x512.jpg')
+  ]
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
--- src/app/recipes/recipes.model.ts
+++ src/app/recipes/recipes.model.ts
@@ -1,4 +1,3 @@
 export class Recipe {
-  constructor(public name: string, public description: string, public imagePath: string) {
-  }
+  constructor(public name: string, public description: string, public imagePath: string) {}
 }
