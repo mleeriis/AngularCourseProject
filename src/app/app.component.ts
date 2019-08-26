d7c1bf96b3eae544149cb20de74b75b5f5d7eb34
mleeriis <mlee@riis.com>
2019-08-26 10:32:43 -0400

    Added Navigation Functionality and Dropdown functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularCourseProject';
+  loadedPage = 'recipes';
+
+  navigateTo(targetPage: string){
+    this.loadedPage = targetPage;
+  }
 }
